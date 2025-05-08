82fcc1f061b25c5d46f08ba5c904351e30c013e6
Siotonic <contact@siotonic.com>
2025-05-08 21:52:13 +0200

    feat(neovim): install lazy plugin manager

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,10 +1,16 @@
--- Line numbering
+-- Options
 vim.opt.relativenumber = true
 vim.opt.number = true
 
--- Indentation settings
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
--- Scroll offset
 vim.opt.scrolloff = 5
+
+-- Keybindings
+vim.g.mapleader = " "
+
+vim.keymap.set('n', '<leader>p', vim.cmd.Ex)
+
+-- Plugins
+require('config.lazy')
