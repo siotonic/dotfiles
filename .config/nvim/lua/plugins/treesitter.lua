d833afb783ff138af899a8047d6e0272953c822f
Siotonic <contact@siotonic.com>
2025-05-08 21:55:15 +0200

    feat(neovim): install treesitter plugin to improve highlighting

---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,36 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function() 
+        local configs = require("nvim-treesitter.configs")
+
+        configs.setup({
+            ensure_installed = {
+                -- Basics
+                "vim",
+                "vimdoc",
+                "markdown",
+                "markdown_inline",
+                "make",
+                -- Configuration
+                "json",
+                "yaml",
+                "toml",
+                "hyprlang",
+                -- Programming
+                "bash",
+                "fish",
+                "lua",
+                "html",
+                "css",
+                "javascript",
+                "typescript",
+                "rust",
+                "go",
+                "gomod",
+            },
+            highlight = { enable = true },
+            indent = { enable = true },
+        })
+    end
+}
