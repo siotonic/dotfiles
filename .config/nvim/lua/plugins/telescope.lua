9fe429d4c3e9e9190b03f5d5386b7637ee03ab4b
Siotonic <contact@siotonic.com>
2025-05-08 21:55:53 +0200

    feat(neovim): install telescope plugin for quick search

---
+++ .config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,30 @@
+return {
+    'nvim-telescope/telescope.nvim', branch = '0.1.x',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+        local telescope = require('telescope')
+        local telescope_config = require('telescope.config')
+        local vimgrep_arguments = { unpack(telescope_config.values.vimgrep_arguments) }
+
+        table.insert(vimgrep_arguments, '--hidden')
+        table.insert(vimgrep_arguments, '--glob')
+        table.insert(vimgrep_arguments, '!**/.git/*')
+
+        telescope.setup({
+            defaults = {
+                vimgrep_arguments = vimgrep_arguments,
+            },
+            pickers = {
+                find_files = {
+                    find_command = { 'rg', '--files', '--hidden', '--glob', '!**/.git/*' },
+                },
+            },
+        })
+
+        local builtin = require('telescope.builtin')
+
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+        vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+        vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+    end
+}
