14edc27a38f1ab9c6834b8a13fd631dca30c470c
Siotonic <contact@siotonic.com>
2025-05-09 02:09:15 +0200

    feat(neovim): improve keybindings (moving lines and auto center view)

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -13,7 +13,16 @@ vim.opt.list = true
 -- Keybindings
 vim.g.mapleader = " "
 
-vim.keymap.set('n', '<leader>p', vim.cmd.Ex)
+vim.keymap.set("n", "<leader>p", vim.cmd.Ex)
+
+vim.keymap.set("v", "<S-Up>", ":move '<-2<CR>gv=gv")
+vim.keymap.set("v", "<S-Down>", ":move '>+1<CR>gv=gv")
+
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
 
 -- Plugins
 require('config.lazy')
