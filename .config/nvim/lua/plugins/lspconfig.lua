f84679b62ac1342d5959a3fc156e5020afde8b6a
Siotonic <contact@siotonic.com>
2025-05-12 02:11:31 +0200

    feat(neovim): add lsp auto completion

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -2,7 +2,9 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
         local lspconfig = require("lspconfig")
-        lspconfig.lua_ls.setup({})
+        local capabilities = require("blink.cmp").get_lsp_capabilities()
+
+        lspconfig.lua_ls.setup({ capabilities = capabilities })
 
         vim.keymap.set("i", "<C-Space>", "<C-x><C-o>")
     end,
