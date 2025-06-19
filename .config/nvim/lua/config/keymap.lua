987932ae866f8db0780d66d5f58f45c303765916
Siotonic <contact@siotonic.com>
2025-06-19 22:53:42 +0200

    feat(nvim): add keybind to copy to clipboard

--- .config/nvim/lua/config/keymap.lua
+++ .config/nvim/lua/config/keymap.lua
@@ -1,6 +1,7 @@
 vim.g.mapleader = " "
 
 vim.keymap.set("i", "<C-Bs>", "<C-w>")
+vim.keymap.set("v", "<leader>y", '"+y')
 
 vim.keymap.set("v", "<S-Up>", ":move '<-2<CR>gv=gv")
 vim.keymap.set("v", "<S-Down>", ":move '>+1<CR>gv=gv")
