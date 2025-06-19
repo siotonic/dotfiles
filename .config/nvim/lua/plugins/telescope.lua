a706e49a607fe72d8e4981a3d65934a7e1bcc06d
Siotonic <contact@siotonic.com>
2025-06-19 22:36:20 +0200

    feat(nvim): add telescope plugin to find files and find in files

---
+++ .config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,22 @@
+return {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+        defaults = {
+            vimgrep_arguments = { "rg", "--color=never", "--no-heading", "--with-filename", "--line-number", "--column", "--smart-case", "--hidden", "--glob", "!**/.git/*" },
+        },
+        pickers = {
+            find_files = {
+                find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+            },
+        },
+    },
+    keys = {
+        { "<leader>ff", ":Telescope find_files<CR>", desc = "Telescope find files" },
+        { "<leader>fg", ":Telescope live_grep<CR>", desc = "Telescope live grep" },
+        { "<leader>fb", ":Telescope buffers<CR>", desc = "Telescope buffers" },
+    },
+}
